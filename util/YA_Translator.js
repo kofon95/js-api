c61b9ec6ebb1817f49d7b6634aba6b1f2cd07b2d
wetywery qwrwqet <wetywery qwrwqet>
2015-10-25 17:32:21 +0300

    added YA_Translator.js

---
+++ util/YA_Translator.js
@@ -0,0 +1,39 @@
+"use strict"
+// using jQuery (ajax)
+
+var YA_Translator;
+// getLang: return promise
+// translate: return promise
+
+(function(){
+	var key = "trnsl.1.1.20151020T033847Z.922fc037e11613df.7952c12e9932e591e6b9a28a129ee588fdd94515";
+
+	YA_Translator.getLang = function(text){
+		return new Promise(function(resolve, reject){
+			$.ajax({
+				url: "https://translate.yandex.net/api/v1.5/tr.json/detect?text=" + text + "&key=" + key,
+				success: function(res){
+					resolve(res["lang"]);
+				},
+				error: function(err){
+					reject(err);
+				}});
+		});
+	}
+
+	YA_Translator.translate = function(text, lang){
+		lang = lang || "en";
+
+		return new Promise(function(resolve, reject){
+			$.ajax({
+				url: "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + key + "&text=" + text + "&lang=" + lang + "&options=1",
+				success: function(res){
+					resolve(res);
+				},
+				error: function(err){
+					reject(err);
+				}
+			});
+		})
+	}
+})()
